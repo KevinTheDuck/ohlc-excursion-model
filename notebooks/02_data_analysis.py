beae7edcb1bce822e5d694d8aad529298c1261cb
Kevin Nathaniel <dchristov2005206@gmail.com>
2026-03-27 20:51:32 +0700

    added violin plot of candle ranges and analysis

--- notebooks/02_data_analysis.py
+++ notebooks/02_data_analysis.py
@@ -1,5 +1,10 @@
 # %%
+from datetime import date
+
+import matplotlib.pyplot as plt
+import pandas as pd
 import polars as pl
+import seaborn as sns
 
 from ohlc_dss_model.data.integrity import sort_data
 from ohlc_dss_model.utils.dt_utils import (
@@ -30,7 +35,13 @@ print(df.filter(df.is_duplicated()))
 # Next we will handle session tagging which we will use to seperate different trading days
 # Or simply we just define which row belongs to a daily candle
 df = session_tagging(df)
+
+# since these dates doesnt have a complete day (cutoff from the dataset) we will ommit these
+df = df.filter(
+    (pl.col("Session") != date(2016, 11, 15)), (pl.col("Session") != date(2025, 10, 1))
+)
 print(df.select(["DateTime", "Session"]).head(5))
+print(df.select(["DateTime", "Session"]).tail(5))
 
 # %%
 # Next we will also need to tag intraday session for each candle such as asia london and new york
@@ -59,3 +70,40 @@ descriptive_analysis = descriptive_stats.group_by("Intraday_Session").agg(
 )
 
 print(descriptive_analysis.filter(pl.col("Intraday_Session") != "Closed"))
+# What we can get from this data? both std deviation and mean candle ranges for new york
+# are ~2 times the amount of london and asia these proofs Andersen-Bollerslev intraday periodicity hypothesis
+# The return distribution of the dataset across all session astronomical values (86, 170, 193) shows clear
+# leptokurtic behavior this means there are events such as the black swan events frequently present
+# meaning that simple linear models are insufficient.
+
+# %%
+# we will do violin plot to better visualize cnadle ranges, but we have a problem
+# we see that our maximum candle range is such a big number in comparison to the mean it would dwarf
+# the rest of the candles in the plot so we have to essentially treat these as an outlier and ommit them
+# in the plotting process, we can ommit them by just filtering the 99 precentile
+limit = descriptive_stats["Candle_Range"].quantile(0.99)
+candle_range_plot = descriptive_stats.filter(pl.col("Candle_Range") < limit)
+candle_range_plot = candle_range_plot.to_pandas()
+
+plt.figure(figsize=(12, 8))
+
+sns.violinplot(
+    data=candle_range_plot,
+    x="Intraday_Session",
+    y="Candle_Range",
+    order=["Asia", "London", "New York"],
+    inner="quart",
+    density_norm="width",
+    palette="dark:#5A9_r",
+    hue="Intraday_Session",
+)
+
+plt.title(
+    "Distribution of 30 Minute Candle Ranges Aggregated by Intraday Sessions (NAS100)"
+)
+plt.ylabel("Candle Range (High/Low)")
+plt.xlabel("Intraday Session")
+plt.show()
+# Based on this we can clearly see the volatility differences between each session, where new york session
+# shows clear lead where the belly are much higher and wider, also it has much higher median compared to other session
+# whereas asia and london are much more squashed, this essentially just tells us that new york have more candles that are larger.
